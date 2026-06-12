672ca48016929ab9065a04fa45b778569831ba28
bechovang <phuchcm2006@gmail.com>
2026-06-13 03:00:58 +0700

    Add migration translating subjects to Vietnamese

---
+++ supabase/migrations/20260613000001_translate_subjects_to_vi.sql
@@ -0,0 +1,6 @@
+-- Translate subject names to Vietnamese
+update public.subjects set name = 'Toán học' where name = 'Math';
+update public.subjects set name = 'Tiếng Anh' where name = 'English';
+update public.subjects set name = 'Vật lý' where name = 'Physics';
+update public.subjects set name = 'Hóa học' where name = 'Chemistry';
+update public.subjects set name = 'Lập trình' where name = 'Programming';
